9a056db832b501719b09a74077568fb1e12ea5f2
JRR Guille <pinchoskk@gmail.com>
2026-07-14 17:43:20 -0300

    Add controlled background video playback

---
+++ background.js
@@ -0,0 +1,35 @@
+(() => {
+  "use strict";
+
+  const video = document.querySelector("#site-background");
+  if (!video) return;
+
+  const PLAYBACK_RATE = 0.78;
+
+  const applyPlaybackRate = () => {
+    video.defaultPlaybackRate = PLAYBACK_RATE;
+    video.playbackRate = PLAYBACK_RATE;
+  };
+
+  applyPlaybackRate();
+  video.addEventListener("loadedmetadata", applyPlaybackRate);
+  video.addEventListener("play", applyPlaybackRate);
+
+  const reducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+  const syncMotionPreference = () => {
+    if (reducedMotion.matches) {
+      video.pause();
+      video.currentTime = 0;
+    } else {
+      applyPlaybackRate();
+      const playAttempt = video.play();
+      if (playAttempt && typeof playAttempt.catch === "function") {
+        playAttempt.catch(() => {});
+      }
+    }
+  };
+
+  reducedMotion.addEventListener?.("change", syncMotionPreference);
+  syncMotionPreference();
+})();
